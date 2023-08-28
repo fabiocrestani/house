7c9e7365c6116a77280c91206b09e965237690ba
Fabio Crestani <crestani.fabio@gmail.com>
2023-08-28 21:20:40 +0200

    Add more

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-CMD [ "python", "./main.py" ]
+CMD [ "uvicorn", "main:app" , "--reload", "--host", "0.0.0.0", "--port", "3000"]
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print("Hello world!")
+
+from fastapi import FastAPI
+
+
+def read_temperature():
+    f = open("/sys/class/thermal/thermal_zone0/temp", "r")
+    value = 0
+    try:
+        value = int(f.read())
+        value = value / 1000
+    except:
+        pass
+    return value
+
+
+app = FastAPI()
+read_temperature()
+
+@app.get("/")
+async def root():
+    t = read_temperature()
+    return {
+        "temperature_cpu": t
+    }
